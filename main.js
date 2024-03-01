519fff26704ed040dd9f795d3c6eb4fda8b90649
ritvik130 <rjoha091@uottawa.ca>
2024-03-01 17:51:13 -0500

    feat: implemented the weather api

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+import { getWeather } from "./weather.js"
 
-
+const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+getWeather(10, 10, timeZone)
+    .then(data => {
+        console.log(data);
+    }
+);
